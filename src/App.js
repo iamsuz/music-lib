f336b4148c7dadc4857f5890631d45be6c41a104
Sujit M <s.mahavarkar@gmail.com>
2021-05-04 19:49:04 +0700

    adds link to to library

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import MusicTable from "./components/MusicTable";
@@ -6,10 +5,12 @@ import MusicTable from "./components/MusicTable";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Landing} />
-        <Route path="/library" exact component={MusicTable} />
-      </Switch>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/library" exact component={MusicTable} />
+        </Switch>
+      </div>
     </Router>
   );
 }
